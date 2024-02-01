0d4171375f8aceb9488ebab72f3c3688dd31a266
Artem <cornilov00@gmail.com>
2024-02-01 23:13:49 +0300

    feat: убрал videos из github

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -2,10 +2,60 @@
 
 import { getSession, signOut } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import Show from '@/app/_widget/Show/Show';
 import { Session } from 'next-auth';
 import { useCurrentUser } from '@/app/hooks/useCurrentUser';
+import AnimeList from '@/app/home/component/AnimeList';
+
+import { axiosInstance } from '@/app/api/axios/axiosInstans';
+import useSWR from 'swr';
+
+type AnimeRequestParams = {
+  page?: number;
+  limit?: number;
+  type?:
+    | 'tv'
+    | 'movie'
+    | 'ova'
+    | 'special'
+    | 'ona'
+    | 'music'
+    | 'cm'
+    | 'pv'
+    | 'tv_special';
+  status?: 'airing' | 'complete' | 'upcoming';
+  order_by:
+    | 'mal_id'
+    | 'title'
+    | 'start_date'
+    | 'end_date'
+    | 'episodes'
+    | 'score'
+    | 'scored_by'
+    | 'rank'
+    | 'popularity'
+    | 'members'
+    | 'favorites';
+};
+
+const animeFetcher = async ({
+  page,
+  limit,
+  status,
+  order_by,
+}: AnimeRequestParams) => {
+  return await axiosInstance
+    .get('https://api.jikan.moe/v4/anime', {
+      params: {
+        page: page ?? 1,
+        limit: limit ?? 24,
+        status: status ?? 'airing',
+        order_by: order_by ?? 'popularity',
+      },
+    })
+    .then((res) => res.data);
+};
 
 const Home = () => {
   const [session, setSession] = useState<Session | null>(null);
@@ -20,19 +70,37 @@ const Home = () => {
     });
   }, [getSession]);
 
-  console.log(currentUser);
+  const [page, setPage] = useState(1);
+
+  const pagePlusOneHandler = useCallback(() => {
+    setPage((prevState) => prevState + 1);
+  }, []);
+
+  const pageMinusOneHandler = useCallback(() => {
+    setPage((prevState) => prevState - 1);
+  }, []);
+
+  const { data: animes } = useSWR(
+    { page, limit: 24, status: 'airing', order_by: 'popularity' },
+    animeFetcher,
+  );
 
   return (
     <Show when={!!session}>
-      <div className="text-white">{`Добро пожаловать, ${currentUser?.name}`}</div>
-      <button
-        className="w-full h-24 bg-white"
-        onClick={async () => {
-          await signOut({ callbackUrl: '/auth' });
-        }}
-      >
-        ВЫЙТИ
-      </button>
+      <div className="w-full min-h-[calc(50.25vw-147px)]">
+        <video
+          autoPlay={true}
+          muted
+          loop={true}
+          src="/videos/videoAnime.mp4"
+          className={
+            'w-full absolute top-0 object-cover brightness-50 h-[50.25vw]'
+          }
+        ></video>
+      </div>
+      <div className={'px-[48px] py-[32px]'}>
+        <AnimeList animeList={animes?.data} listTitle="Онгоинги" />
+      </div>
     </Show>
   );
 };
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,16 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/effect-fade';
+import { Navigation, Pagination, EffectFade } from 'swiper/modules';
+
+//@NOTE https://stackoverflow.com/questions/50009818/javascript-swiper-native-navigation-function-is-not-working
+import SwiperCore from 'swiper';
+SwiperCore.use([Navigation, Pagination, EffectFade]);
+
 const inter = Inter({ subsets: ['latin'] });
 
 export default function RootLayout({
